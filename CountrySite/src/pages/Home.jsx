c2a4f65e77fd20fa8da15ec0061fd7a47ac3dbb0
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-03-26 13:59:47 -0300

    V2

--- CountrySite/src/pages/Home.jsx
+++ CountrySite/src/pages/Home.jsx
@@ -1,44 +1,50 @@
 import { useState, useEffect } from "react";
 
 function Home() {
-  const [todosPaises, setTodosPaises] = useState([]);
+    const [todosPaises, setTodosPaises] = useState([]);
 
-  const [nomePaisEspecifico, setNomePaisEspecifico] = useState(() => {
-    const salvarNomePais = window.localStorage.getItem('Países');
-    return salvarNomePais ? JSON.parse(salvarNomePais) : 'Brazil';
-  });
+    const [nomePaisEspecifico, setNomePaisEspecifico] = useState(() => {
+        const salvarNomePais = window.localStorage.getItem('Países');
+        return salvarNomePais ? JSON.parse(salvarNomePais) : 'Brazil';
+    });
 
-  const [paisEspecifico, setPaisEspecifico] = useState([]);
-  const [error, setError] = useState(null);
+    const [paisEspecifico, setPaisEspecifico] = useState(() => {
+        const salvarPais = window.localStorage.getItem('Pais Específico Salvo');
+        return salvarPais ? JSON.parse(salvarPais) : [];
+    });
 
-  useEffect(() => {
-    async function TodosOsPaises() {
-      try {
-        const respostaTodos = await fetch("https://restcountries.com/v3.1/independent?status=true&fields=name&quot");
+    const [error, setError] = useState(null);
 
-        if (!respostaTodos.ok) {
-          throw new Error("Viadão bem legalzão");
+    useEffect(() => {
+        async function TodosOsPaises() {
+            try {
+                const respostaTodos = await fetch("https://restcountries.com/v3.1/independent?status=true&fields=name&quot");
+
+                if (!respostaTodos.ok) {
+                    throw new Error("Viadão bem legalzão");
+                }
+
+                const dadosTodos = await respostaTodos.json();
+                setTodosPaises(dadosTodos);
+
+            } catch (error) {
+                setError(error.message);
+            }
         }
 
-        const dadosTodos = await respostaTodos.json();
-        setTodosPaises(dadosTodos);
+        TodosOsPaises();
+    }, []);
 
-      } catch (error) {
-        setError(error.message);
-      }
-    }
+    console.log(todosPaises);
 
-    TodosOsPaises();
-  }, []);
-
-  console.log(todosPaises);
-
-  useEffect(() => {
+useEffect(() => {
     async function PaisEspecificoEncontrar() {
         try {
-            const respostaPaisEspecifico = await fetch(`https://restcountries.com/v3.1/name/${nomePaisEspecifico}`)
+            const respostaPaisEspecifico = await fetch(`https://restcountries.com/v3.1/name/${nomePaisEspecifico}?fullText=true`)
 
             localStorage.setItem("Países", JSON.stringify(nomePaisEspecifico));
+            
+            localStorage.setItem("Pais Específico Salvo", JSON.stringify(salvarPais));
 
             if (!respostaPaisEspecifico.ok) {
                 throw new Error("Viadão bem legalzão");
@@ -54,47 +60,47 @@ function Home() {
     }
 
     PaisEspecificoEncontrar();
-  }, [nomePaisEspecifico]);
+}, [nomePaisEspecifico]);
 
-  console.log("Pais especifico: ", paisEspecifico)
+console.log("Pais especifico: ", paisEspecifico)
 
-  console.log(error)
+console.log(error)
 
-  return (
+return (
     <>
-      <p>Kawaii</p>
+        <p>Kawaii</p>
 
-      <select
-        onChange={(e) => {
-          setNomePaisEspecifico(e.target.value);
-        }}
-        value={nomePaisEspecifico}
-      >
-        {todosPaises.map((pais) => {
-          return (
-            <option key={pais.latlng} value={pais.name.common}>
-              {pais.name.common}
-            </option>
-          );
-        })}
-      </select>
+        <select
+            onChange={(e) => {
+                setNomePaisEspecifico(e.target.value);
+            }}
+        >
+            <option>Select One</option>
+            {todosPaises.map((pais) => {
+                return (
+                    <option key={pais.latlng} value={pais.name.official}>
+                        {pais.name.common}
+                    </option>
+                );
+            })}
+        </select>
 
-      <div>
-        {paisEspecifico.map((dadosDosPaises) => {
-            return (
-                <div key={dadosDosPaises.latlng}>
-                    <h2>{dadosDosPaises.name.common}</h2>
-                    <img src={dadosDosPaises.flags.png} alt={dadosDosPaises.name.common} />
-                    <p>Capital: {dadosDosPaises.capital}</p>
-                    <p>População: {dadosDosPaises.population}</p>
-                    <p>continents: {dadosDosPaises.continents}</p>
-                    <p>Area: {dadosDosPaises.area}km²</p>
-                </div>
-            )
-        })}
-      </div>
+        <div>
+            {paisEspecifico.map((dadosDosPaises) => {
+                return (
+                    <div key={dadosDosPaises.latlng}>
+                        <h2>{dadosDosPaises.name.common}</h2>
+                        <img src={dadosDosPaises.flags.png} alt={dadosDosPaises.name.common} />
+                        <p>Capital: {dadosDosPaises.capital}</p>
+                        <p>População: {dadosDosPaises.population}</p>
+                        <p>continents: {dadosDosPaises.continents}</p>
+                        <p>Area: {dadosDosPaises.area}km²</p>
+                    </div>
+                )
+            })}
+        </div>
 
     </>
-  );
+);
 }
 export default Home;
